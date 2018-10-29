792ea153b9641609eb068c7cccf832023017dcbd
Rafael Raygoza <rafael.raygoza@rainde.net>
2018-10-29 13:28:26 -0600

    Enum Class

---
+++ DndCharacter/BL.BusinessLogic/Collections/ClassesColecction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BL.BusinessLogic.Collections
+{
+    public enum ClassesCollection
+    {
+        Wizard = 1,
+        Warlock = 2,
+        Monk = 3,
+        Figther = 4,
+        Barbarian = 5,
+        Bard = 6,
+        Sorcerer = 7
+    }
+}
---
+++ DndCharacter/BL.BusinessLogic/ViewModel/CharacterViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BL.BusinessLogic.Collections;
+
+namespace BL.BusinessLogic.ViewModel
+{
+    public class CharacterViewModel
+    {
+        public string Name { get; set; }
+        public ClassesCollection Class { get; set; }
+    }
+}
