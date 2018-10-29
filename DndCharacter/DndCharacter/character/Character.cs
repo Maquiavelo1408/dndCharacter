4f9fbc7c1ddb0ddc18779f025071aad1c6974ecf
Rafael Raygoza <rafael.raygoza@rainde.net>
2018-10-29 13:03:39 -0600

    CharacterClass

---
+++ DndCharacter/DndCharacter/character/Character.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace DndCharacter
+{
+    public class Character
+    {
+        public Character()
+        {
+            GetData();
+        }
+        public void GetData()
+        {
+            var data = JObject.Parse(File.ReadAllText("@./propertis.json"));
+        }
+    }
+}
