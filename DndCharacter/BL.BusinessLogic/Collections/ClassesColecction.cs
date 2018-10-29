f47ffb2199c897a6de81dd6ea3b5e56c180343f0
Rafael Raygoza <rafael.raygoza@rainde.net>
2018-10-29 13:44:52 -0600

    Classes variant

--- DndCharacter/BL.BusinessLogic/Collections/ClassesColecction.cs
+++ DndCharacter/BL.BusinessLogic/Collections/ClassesColecction.cs
@@ -14,4 +14,16 @@ namespace BL.BusinessLogic.Collections
         Bard = 6,
         Sorcerer = 7
     }
+    public enum WizardVariants
+    {
+        Transmutation = 1,
+        Adivination = 2,
+        Necromancer = 3
+    }
+
+    public enum FigtherVariants
+    {
+        PrimalPath = 1
+
+    }
 }
